bb0b1ebb7f4069c87f4120f72f0154f019874f5b
hkidh <hki.dh4@gmail.com>
2021-06-15 15:16:05 +0800

    calc avg_v2

--- read.py
+++ read.py
@@ -5,10 +5,11 @@ with open('reviews.txt', 'r') as f: #開檔案
 	for line in f:					#一筆筆
 		data.append(line)			#每筆加入data[]清單
 		count += 1 					#計數每一筆(疊加)
-		if count % 10000 == 0: # %餘數 count 如果是1000的倍數
-			x = len(data)			#總筆數
+#		if count % 10000 == 0: # %餘數 count 如果是1000的倍數
+		x = len(data)			#總筆數
 print('總共有', x, '筆')
 
+print(data[0])
 sum_len = 0  					#初終值
 for d in data:					#data(清單)中每個
 	sum_len += len(d) 			#計數清單中的字數
