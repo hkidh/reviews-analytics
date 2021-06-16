229051df98c5815bb4f1e6609dc56ad802d08c4b
hkidh <hki.dh4@gmail.com>
2021-06-17 07:28:41 +0800

    add word count

--- read.py
+++ read.py
@@ -14,7 +14,7 @@ print('總共有', x, '筆')
 new = []
 for d in data:					#data(清單)中每個
 	if len(d) < 100:
-		new.append(d)
+ 		new.append(d)
 print('小於100字的留言有', len(new), '筆')
 print(new[0])		
 #	sum_len += len(d) 			#計數清單中的字數
@@ -22,7 +22,60 @@ print(new[0])
 
 good = []
 for d in data:
-	if 'good' in d:
-		good.append(d)
+ 	if 'good' in d:
+ 		good.append(d)
 print('一共有', len(good), '筆提到good')
 print(good[0]) 
+
+
+
+
+
+#文字計數
+
+w_c = {} # word_count
+for d in data:  			#將一筆一筆的讀
+	words = d.split() 	#用空分出來的字,spolt預設即是空白鍵,不需(' ')
+	for word in words:    	#將一個字一個字的讀
+		if word in w_c:		#如果字有在字典中
+			w_c[word] += 1	#將字在字典的數+1
+		else:
+			w_c[word] = 1 	#新增新的key進w_c字典
+
+for word in w_c:
+	if w_c[word] > 1000000:
+		print(word, w_c[word])
+
+print('出現過的字: ', len(w_c))
+while True:
+	keyin = input('請輸入要查詢的字: ')
+	if keyin == 'q':
+		break
+	if keyin in w_c:
+		print(keyin,'出現的次數為: ', w_c[keyin])
+	else:
+		print('查沒有這個字!!')
+
+print('Thank You')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
